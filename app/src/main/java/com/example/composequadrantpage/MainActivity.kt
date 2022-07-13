9f6bb75330d2fb20c3a73b5923a767537bf3d119
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-13 11:56:18 +0300

    added background to Text COmposable and reworkedsome strings

--- app/src/main/java/com/example/composequadrantpage/MainActivity.kt
+++ app/src/main/java/com/example/composequadrantpage/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.composequadrantpage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrantpage.ui.theme.ComposeQuadrantPageTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,24 +47,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TextComposable(headingText:String, bodyText:String) {
-    Column {
-        Text(text = headingText)
-        Text(text = bodyText)
+    Column(
+        Modifier.background(Color.Green),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = headingText,
+            modifier = Modifier.padding(bottom =16.dp),
+            fontWeight = FontWeight.Bold,
+//            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = bodyText,
+            textAlign = TextAlign.Justify
+            )
     }
 }
 
 @Composable
 fun ImageComposable(headingImg:String, bodyTextImg:String) {
     Column {
-        Text(text = headingImg)
+        Text(
+            text = headingImg,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom =16.dp)
+        )
         Text(text = bodyTextImg)
     }
 }
 
 @Composable
 fun RowComposable(headingRow:String, bodyTextRow:String) {
-    Column {
-        Text(text = headingRow)
+    Column() {
+        Text(
+            text = headingRow,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom =16.dp)
+        )
         Text(text = bodyTextRow)
     }
 }
@@ -68,7 +92,11 @@ fun RowComposable(headingRow:String, bodyTextRow:String) {
 @Composable
 fun ColumnComposable(headingColumn:String, bodyTextColumn:String) {
     Column {
-        Text(text = headingColumn)
+        Text(
+            text = headingColumn,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom =16.dp)
+            )
         Text(text = bodyTextColumn)
     }
 }
@@ -80,13 +108,17 @@ fun ComposeQuadrant(
     headingRow: String, bodyTextRow: String,
     headingColumn: String, bodyTextColumn: String
 ) {
-    Row() {
-        TextComposable(headingText = headingText, bodyText = bodyText)
-        ImageComposable(headingImg = headingImg, bodyTextImg = bodyTextImg)
-    }
-    Row() {
-        RowComposable(headingRow = headingRow, bodyTextRow = bodyTextRow)
-        ColumnComposable(headingColumn = headingColumn, bodyTextColumn = bodyTextColumn)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row() {
+            TextComposable(headingText = headingText, bodyText = bodyText)
+            ImageComposable(headingImg = headingImg, bodyTextImg = bodyTextImg)
+            RowComposable(headingRow = headingRow, bodyTextRow = bodyTextRow)
+            ColumnComposable(headingColumn = headingColumn, bodyTextColumn = bodyTextColumn)
+        }
     }
 }
 
