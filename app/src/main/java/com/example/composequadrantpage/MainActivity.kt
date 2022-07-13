a38b6190452e81848397528d731582a7718105ec
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-13 18:45:03 +0300

    quadrant app

--- app/src/main/java/com/example/composequadrantpage/MainActivity.kt
+++ app/src/main/java/com/example/composequadrantpage/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//generic info card
+//reusable InfoCard
 @Composable
 fun InfoCard(
     title:String,
@@ -106,122 +106,5 @@ fun ComposeQuadrantPage() {
 fun ComposeQuadrantPreview() {
     ComposeQuadrantPageTheme {
         ComposeQuadrantPage()
-        /*
-        ComposeQuadrant(
-            headingText = stringResource(R.string.text_heading),
-            bodyText = stringResource(R.string.text_body),
-            headingImg = stringResource(R.string.image_heading),
-            bodyTextImg = stringResource(R.string.image_body),
-            headingRow = stringResource(R.string.row_heading),
-            bodyTextRow = stringResource(R.string.row_body),
-            headingColumn = stringResource(R.string.column_heading),
-            bodyTextColumn = stringResource(R.string.column_body)
-        )
-        */
     }
 }
-
-/*
-@Composable
-fun TextComposable(headingText:String, bodyText:String) {
-    Column(
-        Modifier
-            .background(Color.Green)
-            .width(175.dp)
-            .fillMaxHeight(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = headingText,
-            modifier = Modifier
-                .padding(bottom =16.dp),
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = bodyText,
-            textAlign = TextAlign.Justify
-            )
-    }
-}
-
-@Composable
-fun ImageComposable(headingImg:String, bodyTextImg:String) {
-    Column(
-        Modifier
-            .background(Color.Yellow)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = headingImg,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom =16.dp)
-        )
-        Text(text = bodyTextImg)
-    }
-}
-
-@Composable
-fun RowComposable(headingRow:String, bodyTextRow:String) {
-    Column(
-        Modifier
-            .background(Color.Cyan)
-            .width(175.dp)
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = headingRow,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom =16.dp)
-        )
-        Text(text = bodyTextRow)
-    }
-}
-
-@Composable
-fun ColumnComposable(headingColumn:String, bodyTextColumn:String) {
-    Column(
-        Modifier
-            .background(Color.LightGray)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = headingColumn,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom =16.dp)
-            )
-        Text(text = bodyTextColumn)
-    }
-}
-
-@Composable
-fun ComposeQuadrant(
-    headingText: String,bodyText: String,
-    headingImg: String, bodyTextImg: String,
-    headingRow: String, bodyTextRow: String,
-    headingColumn: String, bodyTextColumn: String
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(modifier = Modifier.height(400.dp)) {
-            TextComposable(headingText = headingText, bodyText = bodyText)
-            ImageComposable(headingImg = headingImg, bodyTextImg = bodyTextImg)
-        }
-        Row(modifier = Modifier.height(400.dp)) {
-            RowComposable(headingRow = headingRow, bodyTextRow = bodyTextRow)
-            ColumnComposable(headingColumn = headingColumn, bodyTextColumn = bodyTextColumn)
-        }
-    }
-}
- */
